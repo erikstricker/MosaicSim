f9ffd24fab177fcb860bfaf9760cc6f293c8f0d9
Erik Stricker <37314125+erikstricker@users.noreply.github.com>
2025-03-26 15:46:54 -0500

    Add files via upload

--- scripts/tykevarsimulator.py
+++ scripts/tykevarsimulator.py
@@ -51,67 +51,57 @@ else:
 if args.minAFsv is None:
     minAFsv=0.01 # minimum allele frequency SV 
 else:
-    minAFsv=args.minAFsv   
+    minAFsv=args.minimum_allele_frequency_sv  
 
 if args.maxAFsv is None:
     maxAFsv=0.05  # maximum allele frequency SV
 else:
-    maxAFsv=args.maxAFsv  
+    maxAFsv=args.maximum_allele_frequency_sv 
 
 if args.minAFsnv is None:
     minAFsnv=0.01 # minimum allele frequency SNV 
 else:
-    minAFsnv=args.minAFsnv   
+    minAFsnv=args.minimum_allele_frequency_snv  
 
 if args.maxAFsnv is None:
     maxAFsnv=0.05  # maximum allele frequency SNV
 else:
-    maxAFsnv=args.maxAFsnv  
+    maxAFsnv=args.maximum_allele_frequency_snv 
 
 if args.numsv is None:
     numsv=50  # number of SVs to simulate
 else:
-    numsv=args.numsv 
-
-if args.numsv is None:
-    numsv=50  # number of SVs to simulate
-else:
-    numsv=args.numsv 
+    numsv=args.number_of_svs 
 
 if args.numsnv is None:
     numsnv=200  # number of SNVs to simulate
 else:
-    numsnv=args.numsnv 
-
-if args.numsnv is None:
-    numsnv=200  # number of SNVs to simulate
-else:
-    numsnv=args.numsnv 
+    numsnv=args.number_of_snvs  
 
 if args.minsvl is None:
     minsvl=50  # minimum SV length
 else:
-    minsvl=args.minsvl 
+    minsvl=args.minimum_sv_length 
 
 if args.maxsvl is None:
     maxsvl=10000  # maximum SV length
 else:
-    maxsvl=args.maxsvl 
+    maxsvl=args.maximum_sv_length 
 
 if args.sub is None:
     sub=1  # probability of producing a SNP vs indel in SNV
 else:
-    sub=args.sub 
+    sub=args.substitution_rate 
 
 if args.insdelsnv is None:
     insdelsnv=0.5  # probability of producing INS vs DEL in SNV
 else:
-    insdelsnv=args.insdelsnv 
+    insdelsnv=args.insdel_snv_rate 
 
 if args.insdel is None:
     insdel=0.7  # probability of producing INS vs DEL in SV
 else:
-    insdel=args.insdel 
+    insdel=args.insdel_sv_rate 
 
 
 SVvcf=f"{output_prefix}_SV.vcf" # name of output vcf file for SV
